36e87cca5ce920d0ad50191d1e7d6d1bac823d48
Claire Boals <claireboals@gmail.com>
2021-07-11 16:45:21 -0400

    Initial unit test

--- src/test/java/challenge/javarestful/JavaRestfulApplicationTests.java
+++ src/test/java/challenge/javarestful/JavaRestfulApplicationTests.java
@@ -1,13 +1,21 @@
 package challenge.javarestful;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaRestfulApplicationTests {
 
+	@Autowired
+	private RootController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
