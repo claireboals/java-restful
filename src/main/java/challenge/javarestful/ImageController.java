77c2959809eb3a315ad41d6770b0e6fe61fa52da
Claire Boals <claireboals@gmail.com>
2021-07-12 15:24:13 -0400

    Image Controller and unit tests

---
+++ src/main/java/challenge/javarestful/ImageController.java
@@ -0,0 +1,34 @@
+package challenge.javarestful;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class ImageController {
+    
+    private final ImageRepository repository;
+
+    ImageController(ImageRepository repository){
+        this.repository = repository;
+    }
+
+    // gets all images
+    @GetMapping("/images")
+    List<Image> all(){
+        return repository.findAll();
+    }
+
+    // create a new image
+    @PostMapping(path="/images", consumes="application/json", produces="application/json")
+    Image newImage(@RequestBody Image newImage){
+        return repository.save(newImage);
+    }
+
+}
---
+++ src/test/java/challenge/javarestful/ImageTests.java
@@ -0,0 +1,51 @@
+package challenge.javarestful;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ImageTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void getAllTest() throws Exception {
+		this.mockMvc.perform(get("/images")
+            .accept(org.springframework.http.MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk());
+	}
+
+    @Test
+    public void postTest() throws Exception {
+        this.mockMvc.perform((post("/products"))
+            .content(asJsonString(new Image("test-image-title", "test-image-description")))
+            .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+            .accept(org.springframework.http.MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
